bac7f9c92aaec748949bdb3ef8703d7d9c83d90a
Arufff <101610166+Arufff@users.noreply.github.com>
2022-05-22 23:55:50 +0800

    praktikum

--- app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -29,15 +29,6 @@ import com.example.android.marsphotos.overview.PhotoGridAdapter
 /**
  * Updates the data shown in the [RecyclerView].
  */
-@BindingAdapter("listData")
-fun bindRecyclerView(recyclerView: RecyclerView, data: List<MarsPhoto>?) {
-    val adapter = recyclerView.adapter as PhotoGridAdapter
-    adapter.submitList(data)
-}
-
-/**
- * Uses the Coil library to load an image by URL into an [ImageView]
- */
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
@@ -49,14 +40,16 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
     }
 }
 
-/**
- * This binding adapter displays the [MarsApiStatus] of the network request in an image view.  When
- * the request is loading, it displays a loading_animation.  If the request has an error, it
- * displays a broken image to reflect the connection error.  When the request is finished, it
- * hides the image view.
- */
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView,
+                     data: List<MarsPhoto>?) {
+    val adapter = recyclerView.adapter as PhotoGridAdapter
+    adapter.submitList(data)
+}
+
 @BindingAdapter("marsApiStatus")
-fun bindStatus(statusImageView: ImageView, status: MarsApiStatus?) {
+fun bindStatus(statusImageView: ImageView,
+               status: MarsApiStatus?) {
     when (status) {
         MarsApiStatus.LOADING -> {
             statusImageView.visibility = View.VISIBLE
